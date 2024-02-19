d084c20f36189f52f402a7d4ea7f371338f075e7
aminul-islam-niloy <niloyftaminul@gmail.com>
2024-02-19 13:24:36 +0600

    Add project files.

---
+++ Movie Fair/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Movie_Fair.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+    }
+}
